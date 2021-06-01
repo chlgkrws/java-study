13b8fd3eb38b967e115a042504aee1d4c9ff4ac6
chlgkrws <61148082+chlgkrws@users.noreply.github.com>
2021-06-01 11:52:39 +0900

    Steam - Example

--- src/stream/Example.java
+++ src/stream/Example.java
@@ -1,1 +1,22 @@
+package stream;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Example {
 	
+	public static void main(String[] args) {
+		
+		//임의의 수
+		IntStream intStream = new Random().ints();				//매개변수로 유한 스트림을 만들 수 있음.
+		intStream.limit(5).forEach(System.out::println);
+		
+		//람다식 - iterate(), generate()
+		Stream.iterate(0, n -> n+2);			//.forEach(System.out::println);
+		Stream.generate(() -> 1);				//.forEach(System.out::println);
+		
+		
+		
+	}
+}
